878b34649b5fbc14359cf752c565bfad6780254d
Aroa <apons20484@iesjoanramis.org>
2022-11-24 12:10:11 +0100

    Puig els exercicis nº 11,12,13.

---
+++ 13.py
@@ -0,0 +1,24 @@
+# Definició de funcions
+def longitut(a):
+    l = 0
+    for i in a:
+        l +=1
+    return l 
+
+# Aplicació principal
+a = input("Introdueix una cadena: ")
+b = longitut(a)
+print("La longitut ", a, " és ", b)
+
+
+
+
+# (...
+a=[1,2,3,[4,5,7],8,9]
+b= "Pere"
+c=len(a)=>6
+d=len(b)=>4
+x=longitut(a)=>6
+y=longitut(b)=>y
+...) 
+
